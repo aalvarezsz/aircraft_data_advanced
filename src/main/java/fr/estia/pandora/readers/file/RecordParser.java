11e97e1cdaf0352c2832a5a9af7c5ea1a5744801
Brohoya <b.elgart@net.estia.fr>
2023-03-07 22:42:22 +0100

    Implementation of all the data types of records

--- src/main/java/fr/estia/pandora/readers/file/RecordParser.java
+++ src/main/java/fr/estia/pandora/readers/file/RecordParser.java
@@ -19,12 +19,39 @@ public class RecordParser {
 
 	public Record parse(String data) {
 		Record record = null ; 
-	        // get values separated by coma (csv file...)
         String[] values = data.split(",");
+        
         if( values.length > 0 ) {
         	record = new Record();
-        	record.setTimestamp( Double.parseDouble(values[0]));
-        	record.setEnginePower(Double.parseDouble(values[9]));
+        	record.setTimestamp(Double.parseDouble(values[0]));
+        	record.setLongitude(Double.parseDouble(values[1]));
+        	record.setLatitude(Double.parseDouble(values[2]));
+        	record.setAltitude(Double.parseDouble(values[3]));
+        	
+        	record.setRoll(Float.parseFloat(values[4]));
+        	record.setPitch(Float.parseFloat(values[5]));
+        	record.setYaw(Float.parseFloat(values[6]));
+        	record.setHeading(Float.parseFloat(values[7]));
+        	
+        	record.setAir_speed(Double.parseDouble(values[8]));
+        	record.setEngine_0_power(Double.parseDouble(values[9]));
+        	
+        	if(values.length == 16) {
+        		record.setEngine_1_power(Double.parseDouble(values[10]));
+        		record.setTemperature_in(Double.parseDouble(values[11]));
+        		record.setHumidity_in(Double.parseDouble(values[12]));
+        		record.setPressure_in(Double.parseDouble(values[13]));
+        		
+        		record.setHeart_rate(Double.parseDouble(values[14]));
+        		record.setOxygen_mask(Double.parseDouble(values[15]));
+        	} else {
+        		record.setTemperature_in(Double.parseDouble(values[10]));
+        		record.setHumidity_in(Double.parseDouble(values[11]));
+        		record.setPressure_in(Double.parseDouble(values[12]));
+        		
+        		record.setHeart_rate(Double.parseDouble(values[13]));
+        		record.setOxygen_mask(Double.parseDouble(values[14]));
+        	}
         }
 		return record;
 	}
