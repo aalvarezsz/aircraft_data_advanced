b24ede80df0df4816cfa019db23899b56247ece2
Brohoya <b.elgart@net.estia.fr>
2023-03-09 20:44:39 +0100

    engine power fix

--- src/main/java/fr/estia/pandora/analyses/EnginePower.java
+++ src/main/java/fr/estia/pandora/analyses/EnginePower.java
@@ -8,23 +8,25 @@ import fr.estia.pandora.model.Record;
 public class EnginePower {
 	public static double average(Flight flight) {
 		ArrayList<Record> flightRecords = flight.getRecords();
+		boolean hasTwoEngines = flightRecords.get(0).getEngine_1_power() != 0;
 		double enginePowerSum = 0;
 		
 		for (int i = 0; i<flightRecords.size(); i++) enginePowerSum += flightRecords.get(i).getEngine_0_power();
 		
-		return enginePowerSum / flightRecords.size() * 2;
+		return hasTwoEngines ? enginePowerSum / flightRecords.size() * 2 : enginePowerSum / flightRecords.size();
 	}
 	
 	public static double max(Flight flight) {
 		ArrayList<Record> flightRecords = flight.getRecords();
 		double maxEnginePower = flightRecords.get(0).getEngine_0_power();
+		boolean hasTwoEngines = flightRecords.get(0).getEngine_1_power() != 0;
 		
 		for (int i = 1; i<flightRecords.size(); i++) {
 			if(maxEnginePower < flightRecords.get(i).getEngine_0_power()) {
 				maxEnginePower = flightRecords.get(i).getEngine_0_power();
 			};
 		}
-		return maxEnginePower * 2;
+		return hasTwoEngines ? maxEnginePower * 2 : maxEnginePower;
 	}
 
 }
