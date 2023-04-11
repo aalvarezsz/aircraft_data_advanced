4c7fe070c801f828682bede2fc31a910b9db6caf
Xuban <59646791+XubanCeccon@users.noreply.github.com>
2023-04-11 08:59:48 +0200

    a

---
+++ src/main/java/fr/estia/pandora/analyses/FlightPhases.java
@@ -0,0 +1,18 @@
+package fr.estia.pandora.analyses;
+
+import java.util.ArrayList;
+
+import fr.estia.pandora.model.Flight;
+import fr.estia.pandora.model.Record;
+
+public class FlightPhases {
+
+	public static void test(Flight flight) {
+		ArrayList<Record> flightRecords = flight.getRecords();		
+		return flightRecords.get(i).getHeart_rate()
+		
+			
+		}
+	
+	
+}
